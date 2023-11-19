b0e74cf67a5f68894f09fe1a2f7436ca598b453e
Nishil-Agarwal <132306904+Nishil-Agarwal@users.noreply.github.com>
2023-11-19 05:41:27 +0530

    Add files via upload

--- AP_Proj/src/main/java/com/example/ap_proj/Main.java
+++ AP_Proj/src/main/java/com/example/ap_proj/Main.java
@@ -3,10 +3,17 @@ package com.example.ap_proj;
 import javafx.animation.RotateTransition;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
@@ -22,55 +29,130 @@ import java.io.IOException;
 import static java.lang.Thread.sleep;
 
 public class Main extends Application {
+    private Stage stage;
+    private Scene scene;   //gotta remove this temp placeholder screen after deadline 1
+    //just generate new scene by saved score etc. once code is completed and user presses any buttons that require to go back to gamescene
     private Player player;
     private Mountain mount0;
     private Mountain mount1;
     private Mountain mount2;
     private Mountain mount3;
-    private ImageView backg_img;
+    //private ImageView backg_img;
+    private Stick mystick;
+    private long stickstart;
+    private long endtime;
 
-    @Override
-    public void start(Stage primarystage) throws IOException, InterruptedException {
+    public void switchToGameScene(ActionEvent event) throws IOException {
+        Pane root = FXMLLoader.load(getClass().getResource("GameScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scenenew= new Scene(root, 600, 325);
+        addobjs(scenenew);
+        stage.setScene(scenenew);
+        stage.show();
+    }
+
+    public void switchToStartScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("StartScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void exit(ActionEvent event){
+        Alert exitbox = new Alert(Alert.AlertType.CONFIRMATION);
+        exitbox.setTitle("Exit");
+        exitbox.setHeaderText("You're about to exit the Game");
+        exitbox.setContentText("Do you want to exit the Game?");
+        if(exitbox.showAndWait().get()== ButtonType.OK){
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.close();
+            System.out.println("You have exited the Game.");
+        }
+    }
+
+    public void switchToEndScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("EndScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void addobjs(Scene scene) throws FileNotFoundException {
         player=new Player();
         mount0=new Mountain(0);
         mount1=new Mountain(200);
         mount2=new Mountain(450);
         mount3=new Mountain(600);
-        //Player player2=new Player();
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-        Pane root = loader.load();
+        Pane root=(Pane)scene.getRoot();
 
-        backg_img = new ImageView(new Image("file:C:\\Users\\nishi\\Vs_Code_Projects\\IIITD\\SEM_3\\AP\\Assignments\\AP_Proj\\src\\main\\java\\com\\example\\ap_proj\\background.jpg"));
-
-        root.getChildren().add(backg_img);
         root.getChildren().add(player.getplyr());
         root.getChildren().add(mount0.getmount());
         root.getChildren().add(mount1.getmount());
         root.getChildren().add(mount2.getmount());
         root.getChildren().add(mount3.getmount());
 
-        Scene scene1 = new Scene(root, 884, 500);
+    }
+
+    @Override
+    public void start(Stage primarystage) throws IOException, InterruptedException {
+        this.stage=primarystage;
+//        player=new Player();
+//        mount0=new Mountain(0);
+//        mount1=new Mountain(200);
+//        mount2=new Mountain(450);
+//        mount3=new Mountain(600);
+//        //Player player2=new Player();
+
+        FXMLLoader loadhome = new FXMLLoader(getClass().getResource("StartScene.fxml"));
+        Pane roothome = loadhome.load();
+        Scene scenehome = new Scene(roothome, 600, 325);
 
 
-//        Group newg1 = new Group();
-//        newg1.getChildren().add(player.getplyr());
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("GameScene.fxml"));
+//        Pane root = loader.load();
+
+        //backg_img = new ImageView(new Image("file:C:\\Users\\nishi\\Vs_Code_Projects\\IIITD\\SEM_3\\AP\\Assignments\\AP_Proj\\src\\main\\resources\\com\\example\\ap_proj\\projimg.jpg"));
+
+        //root.getChildren().add(backg_img);
+//        root.getChildren().add(player.getplyr());
+//        root.getChildren().add(mount0.getmount());
+//        root.getChildren().add(mount1.getmount());
+//        root.getChildren().add(mount2.getmount());
+//        root.getChildren().add(mount3.getmount());
 //
-//        Scene scene1 = new Scene(newg1, 600, 600);
+//        tempgamescene = new Scene(root, 600, 325);  //Make it local here by inserting a "Scene" at start of line firstly prolly to revert back
 
-        scene1.setOnKeyPressed(e -> {
-            switch (e.getCode()) {
-                case S:
-                    player.flip();
-                    break;
-                case J:
-                    player.move();
-                    break;
-            }
-        });
+//        tempgamescene.setOnMousePressed(e -> {
+//            if (e.isPrimaryButtonDown()){
+//                stickstart=System.currentTimeMillis();
+//            }
+//        });
+//
+//        tempgamescene.setOnMouseReleased(e->{
+//            endtime=System.currentTimeMillis();
+//            Point2D playercoords=player.getplyr().localToScene(player.getplyr().getLayoutX(), player.getplyr().getLayoutY());
+//            double xcoord = playercoords.getX();
+//            double ycoord = playercoords.getY();
+//            mystick=new Stick(xcoord,ycoord,(int)(endtime-stickstart)/6,player);
+//            root.getChildren().add(mystick.getstick());
+//        });
+//
+//        tempgamescene.setOnKeyReleased(e -> {
+//            switch (e.getCode()) {
+//                case S:
+//                    player.flip();
+//                    break;
+//                case J:
+//                    player.move();
+//                    break;
+//            }
+//        });
 
         primarystage.setTitle("Stick Hero");
-        primarystage.setScene(scene1);
+        primarystage.setScene(scenehome);
         primarystage.show();
     }
 
@@ -79,9 +161,12 @@ public class Main extends Application {
         private int width = 50;
         private ImageView plyr;
         private int flipstatus=0;
+        private int score;
+        private int cherries;
+        private int coins;
         Player() throws FileNotFoundException {
             //plyr= new Rectangle(20,200, 30,40);
-            Image character=new Image(new FileInputStream("C:\\Users\\nishi\\Vs_Code_Projects\\IIITD\\SEM_3\\AP\\Assignments\\AP_Proj\\src\\main\\java\\com\\example\\ap_proj\\Stickman.jpg"));
+            Image character=new Image(new FileInputStream("C:\\Users\\nishi\\Vs_Code_Projects\\IIITD\\SEM_3\\AP\\Assignments\\AP_Proj\\src\\main\\java\\com\\example\\ap_proj\\Stickman.png"));
             plyr = new ImageView(character);
             plyr.setFitHeight(this.height);
             plyr.setFitWidth(this.width);
@@ -137,6 +222,32 @@ public class Main extends Application {
             return this.mountain;
         }
     }
+
+    public class Stick{
+        private final int width =5;
+        private int length;
+        private Rectangle stick;
+        private ImageView skin;
+        public Stick(double playerxcoord,double playerycoord,int length,Player player){
+            this.length=length;
+            stick = new Rectangle(playerxcoord+70,playerycoord+255,length, width);
+            this.stick.setFill(Color.DARKORANGE);
+        }
+
+        public Rectangle getstick(){
+            return this.stick;
+        }
+    }
+
+    public class cherries{
+        private final int lenght=20;
+        private final int width=20;
+        private Rectangle cherry;
+        private int rarity_calc(int score){return 0;}
+        private void setvisibility(){};
+    }
+
+
 
 
     public static void main(String[] args) {
