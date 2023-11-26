79d2e0cf09f22b0931bb82060681a3e746837f63
Nishil-Agarwal <132306904+Nishil-Agarwal@users.noreply.github.com>
2023-11-27 04:51:25 +0530

    Update Cherry.java

--- 2022314_2022334/src/main/java/com/example/ap_proj/Cherry.java
+++ 2022314_2022334/src/main/java/com/example/ap_proj/Cherry.java
@@ -1,12 +1,30 @@
 package com.example.ap_proj;
 
-import javafx.scene.shape.Rectangle;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import static java.lang.Math.pow;
 
 public class Cherry{
-    private final int LENGTH=20;
-    private final int WIDTH=20;
-    private Rectangle cherry;
-    public Cherry(){}
-    private int rarity_calc(int score){return 0;}
-    private void setvisibility(){};
+    private final int HEIGHT=35;
+    private final int WIDTH=45;
+    private ImageView cherry;
+
+    Cherry() throws FileNotFoundException {
+        Image cherries=new Image(new FileInputStream("src\\main\\resources\\com\\example\\ap_proj\\Cherry.png"));
+        cherry = new ImageView(cherries);
+        cherry.setFitHeight(this.HEIGHT);
+        cherry.setFitWidth(this.WIDTH);
+    }
+
+    private double rarity_calc(int score){
+        return (100/(pow(3,-score)));
+    }
+
+    public ImageView generate_cherry(){
+        return this.cherry;
+    }
 }
